0777df96e9aa44884b1166f17ea0e333d4875565
Lakshya Singh Kushwah <laksh.sk108@gmail.com>
2026-05-05 14:47:26 +0530

    Leetcode 704 Binary Search Simple; TimeComplexity O(logN) Solution

---
+++ cpp-dsa/04_BinarySearch/704_BinarySearch.cpp
@@ -0,0 +1,33 @@
+/*
+LeetCode : 704  https://leetcode.com/problems/binary-search/
+Problems : Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+You must write an algorithm with O(log n) runtime complexity.
+Pattern : Binary Search 
+Time Complexity: O(logN)
+Space Complexity: O(1)
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+}
+int main(){
+    vector<int> nums = {-1,0,3,5,9,12};
+    int target = 9;
+    cout<<search(nums,target);
+    return 0;
+}
